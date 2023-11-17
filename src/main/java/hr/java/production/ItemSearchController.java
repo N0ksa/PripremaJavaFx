1455bd16f06675a506d0c1c99e83bd98a92923bf
github_N0ksa <leonsarko7@gmail.com>
2023-11-17 11:38:28 +0100

    Učitao podatke za Item.java klasu u tablicu i dodao metodu koja filtrira artikle po imenu koje je uneseno od strane korisnika u grafičkom sučelju.

--- src/main/java/hr/java/production/ItemSearchController.java
+++ src/main/java/hr/java/production/ItemSearchController.java
@@ -4,12 +4,17 @@ import hr.java.production.constants.Constants;
 import hr.java.production.model.Category;
 import hr.java.production.model.Item;
 import hr.java.production.utility.FileReaderUtil;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.util.Callback;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ItemSearchController {
 
@@ -37,13 +42,62 @@ public class ItemSearchController {
 
 
     public void initialize(){
+        itemNameTableColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Item,String>, ObservableValue<String>>() {
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> param) {
+                return new ReadOnlyStringWrapper(param.getValue().getName());
+            }
+        });
 
+        itemCategoryTableColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Item,String>, ObservableValue<String>>() {
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> param) {
+                return new ReadOnlyStringWrapper(param.getValue().getCategory().getName());
+            }
+        });
+
+        itemWidthTableColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Item,String>, ObservableValue<String>>() {
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> param) {
+                return new ReadOnlyStringWrapper(param.getValue().getWidth().toString());
+            }
+        });
+
+        itemHeightTableColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Item,String>, ObservableValue<String>>() {
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> param) {
+                return new ReadOnlyStringWrapper(param.getValue().getHeight().toString());
+            }
+        });
+
+        itemLengthTableColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Item,String>, ObservableValue<String>>() {
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> param) {
+                return new ReadOnlyStringWrapper(param.getValue().getLength().toString());
+            }
+        });
+
+        itemProductionCostTableColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Item,String>, ObservableValue<String>>() {
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> param) {
+                return new ReadOnlyStringWrapper(param.getValue().getProductionCost().toString());
+            }
+        });
+
+        itemSellingPriceTableColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Item,String>, ObservableValue<String>>() {
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> param) {
+                return new ReadOnlyStringWrapper(param.getValue().getSellingPrice().toString());
+            }
+        });
     }
+
+
+
     public void itemSearch(){
         List<Category> categoryList = FileReaderUtil.getCategoriesFromFile();
         List<Item> itemList = FileReaderUtil.getItemsFromFile(categoryList);
 
-        ObservableList<Item> observableItemList = FXCollections.observableList(itemList);
+        String itemName = itemNameTextField.getText();
+
+        List<Item> filtereditemList = itemList.stream()
+                .filter(item -> item.getName().contains(itemName))
+                .collect(Collectors.toList());
+
+        ObservableList<Item> observableItemList = FXCollections.observableList(filtereditemList);
         itemsTableView.setItems(observableItemList);
     }
 }
---
+++ src/main/java/hr/java/production/MenuController.java
@@ -0,0 +1,4 @@
+package hr.java.production;
+
+public class MenuController {
+}
